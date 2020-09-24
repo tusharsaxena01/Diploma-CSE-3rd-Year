3ca27d483975435f104fa73e9a7fde3088a5b747
Tushar Saxena <71825717+tusharsaxena01@users.noreply.github.com>
2020-09-24 23:15:36 +0530

    First PHP Content
    Adding first PHP content onto github so that my friends could access them easily

---
+++ PHP-files/main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="shortcut icon" href="../images/logo.png">
+<link rel="stylesheet" type="text/css" href="../css/default.css">
+<title>PHP Server</title>
+</head>
+<body>
+<center>
+<?php
+echo "Welcome To My PHP Server!";
+?> 
+</center>
+<div class="nav">
+	<a href="../index.html">&lt;&nbsp;Home</a>
+</div>
+<div class="contents">
+	<ol>
+		<li><a href="first.php" class="sel">First</a></li>
+		<li></li>
+	</ol>
+</div>
+</body>
+</html>
