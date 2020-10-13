09064dbf0df12d582d1a6fe06480cf55ef1cc890
Tushar Saxena <71825717+tusharsaxena01@users.noreply.github.com>
2020-10-13 18:31:01 +0530

    Update ascii.py

--- Python/ascii.py
+++ Python/ascii.py
@@ -3,8 +3,10 @@
 #using exception handling to create a program that is short in length
 i = input("Enter the character\n> ")
 try:
-	b=chr(int(i))
+	b = chr(int(i))
 	print(b)
 except:
 	a = ord(i)
 	print(a)
+
+print("Thank You for using...\n\tMade by Tushar Saxena")
