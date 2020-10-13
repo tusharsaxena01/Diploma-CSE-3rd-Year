480df9a231b58f9e8fc6bb79b01f9befe95b06c2
Tushar Saxena <71825717+tusharsaxena01@users.noreply.github.com>
2020-10-13 18:29:22 +0530

    Update ascii.py

--- Python/ascii.py
+++ Python/ascii.py
@@ -1,7 +1,7 @@
 '''Print ascii value of the given value using python'''
 #using two functions ord to convert into a number and chr to convert back into number format
 #using exception handling to create a program that is short in length
-i = input("> ")
+i = input("Enter the character\n> ")
 try:
 	b=chr(int(i))
 	print(b)
